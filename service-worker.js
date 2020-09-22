bb51e4a39fbfc223ceb1bcaafa8e394dd65d8320
alandsilva26 <alandsilva2001@gmail.com>
2020-09-22 19:40:16 +0000

    Deploying to gh-pages from  @ 6df1cb58abc5864fc4d55aa9f1ffa95296ede31e 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5a622a2a277d9584887119be5e8befa6.js"
+  "/alandsilva26.github.io/precache-manifest.9f1e882e7f753701e83fe0166b4211a3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/alandsilva26.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
