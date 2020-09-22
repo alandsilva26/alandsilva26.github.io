d490447bc90df924cee149bc6c80b8cdee14e771
Alan Dsilva <deathbringer2604@gmail.com>
2020-09-23 02:24:53 +0530

    Change url to public url

--- src/pages/Home/header.js
+++ src/pages/Home/header.js
@@ -1,9 +1,18 @@
 import React from "react";
+// import headerImage from "%PUBLIC_URL%/images/astro.jpg";
 
 function Header() {
+  console.log(process.env.PUBLIC_URL);
+  console.log(process.env.PUBLIC_URL + "/images/astro.jpg");
   return (
     <header>
-      <div className="home--header-overlay"></div>
+      <div
+        className="home--header-overlay"
+        style={{
+          backgroundImage: `url("images/astro.jpg")`,
+        }}
+      ></div>
+
       <div className="container">
         <div className="row">
           <div className="col-md-6">
