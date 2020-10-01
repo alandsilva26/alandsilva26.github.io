057be9ae0ebd7bd545fb259deba22fd7f08b4e97
Alan Dsilva <deathbringer2604@gmail.com>
2020-10-01 14:44:35 +0530

    Fix header for small screens

--- src/index.js
+++ src/index.js
@@ -6,22 +6,27 @@ import App from "./App";
 import "bootstrap";
 import "./sass/main.scss";
 
-import * as firebase from "firebase/app";
-import "firebase/analytics";
+// import "./utils";
 
-var firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: "portfolio-9dbc4.firebaseapp.com",
-  databaseURL: "https://portfolio-9dbc4.firebaseio.com",
-  projectId: "portfolio-9dbc4",
-  storageBucket: "portfolio-9dbc4.appspot.com",
-  messagingSenderId: "841600272388",
-  appId: "1:841600272388:web:12314d1260dded0601cd51",
-  measurementId: "G-55E4QT6C4F",
-};
+// import * as firebase from "firebase/app";
+// import "firebase/analytics";
+// import "firebase/database";
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+// var firebaseConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: "portfolio-9dbc4.firebaseapp.com",
+//   databaseURL: "https://portfolio-9dbc4.firebaseio.com",
+//   projectId: "portfolio-9dbc4",
+//   storageBucket: "portfolio-9dbc4.appspot.com",
+//   messagingSenderId: "841600272388",
+//   appId: "1:841600272388:web:12314d1260dded0601cd51",
+//   measurementId: "G-55E4QT6C4F",
+// };
+
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
+// export const db = firebase.firestore();
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,22 +1,33 @@
 import React from "react";
+import Spinner from "../components/spinner";
+import { ProjectContext } from "../project-context";
 
-function About() {
-  return (
-    <div className="about">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-5">
-            loremloremloremloremloremloremloremloremloremloremloremloremlore
-            mloremloremloremloremloremlorem
-          </div>
-          <div className="col-md-7">
-            loremloremloremloremlo remloremlore mloremloremloremloremloremlore
-            mloremloremloremloremloremlorem
-          </div>
+class About extends React.Component {
+  static contextType = ProjectContext;
+
+  render() {
+    const { user, loading } = this.context;
+    return (
+      <div className="about">
+        <div className="container">
+          <div className="row">{loading ? <Spinner /> : this.data(user)}</div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  data = (user) => {
+    return (
+      <>
+        <img src={user.avatarUrl} alt="Alan Avatar" />
+        {this.element(user.name)} {this.element(user.email)}
+      </>
+    );
+  };
+
+  element = (props) => {
+    return <div className="col-md-12">{props}</div>;
+  };
 }
 
 export default About;
