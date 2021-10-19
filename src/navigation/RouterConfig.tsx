03670cb2358226e581fafef007d22061f6c63a2d
Alan Dsilva <alandsilva2001@gmail.com>
2021-10-19 11:01:46 +0530

    remove admin section

--- src/navigation/RouterConfig.tsx
+++ src/navigation/RouterConfig.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import PrivateRoute from "./PrivateRoute";
-import AuthProvider from "../context/AuthContext";
 
 // components
 import Home from "../pages/Home/HomePage";
 import Project from "../pages/Projects/projects";
 import AboutPage from "../pages/About/AboutPage";
 import NoMatch from "../pages/Errors/404";
-// admin
-import AdminHome from "../pages/Admin/AdminHome";
-import AdminLogin from "../pages/AdminLogin/AdminLogin";
 
 const RouterConfig = (): JSX.Element => {
   return (
@@ -18,13 +13,6 @@ const RouterConfig = (): JSX.Element => {
       <Route exact path="/" component={Home} />
       <Route exact path="/projects" component={Project} />
       <Route exact path="/about" component={AboutPage} />
-      <AuthProvider>
-        <PrivateRoute exact path="/admin">
-          <AdminHome />
-        </PrivateRoute>
-        <Route path="/admin/login" component={AdminLogin} />
-      </AuthProvider>
-
       <Route exact path="*" component={NoMatch} />
     </Switch>
   );
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -11,12 +11,27 @@ const HomePage = (): JSX.Element => {
         <div className="container my-5">
           <div className="row">
             <div className="col">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est alias recusandae repellat, molestiae suscipit iste neque excepturi hic quas aperiam ipsa ab voluptas esse error et fugiat voluptates temporibus cum.
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est
+              alias recusandae repellat, molestiae suscipit iste neque excepturi
+              hic quas aperiam ipsa ab voluptas esse error et fugiat voluptates
+              temporibus cum.
             </div>
             <div className="home__about--images col">
-            <img className="img img-fluid home__about--images-1" src="/avatar.jpg" alt="" />
-            <img className="img img-fluid home__about--images-2" src="/media/selfie.jpg" alt="" />
-            <img className="img img-fluid home__about--images-3" src="/media/event.jpg" alt="" />
+              <img
+                className="img img-fluid home__about--images-1"
+                src="/avatar.jpg"
+                alt=""
+              />
+              <img
+                className="img img-fluid home__about--images-2"
+                src="/media/selfie.jpg"
+                alt=""
+              />
+              <img
+                className="img img-fluid home__about--images-3"
+                src="/media/event.jpg"
+                alt=""
+              />
             </div>
           </div>
         </div>
