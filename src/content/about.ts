d510e1e86bcbb33fdaf99bd7cc05ac4de5c9c098
Alan Dsilva <alandsilva2001@gmail.com>
2024-03-10 12:13:14 +0530

    update about

---
+++ src/content/about.ts
@@ -0,0 +1,64 @@
+const about = {
+    name: "Alan Anand Dsilva",
+    description:
+        "👋 Hi I am Alan - a computer science engineer currently pursuing my bachelors degree at St. Francis Institute of Technology. I code for fun and professional reasons. I create software for android and web platforms. In my free time I like to play videogames🎮. Occasionally I watch anime.",
+    avatarUrl:
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-9dbc4.appspot.com/o/alan.jpg?alt=media&token=9deb4003-91df-4250-88ae-b2c326b4652c",
+    location: "Mumbai, India",
+    github: "alandsilva26",
+    linkedin: "https://www.linkedin.com/in/alan-dsilva-289b77170/",
+    codechef: "deathbringer26",
+    hackerrank: "alandsilva2001",
+    steam: "deathbringer269",
+    email: "alandsilva2001@gmail.com",
+    technical: [
+        {
+            name: "Programming Languages",
+            content: [
+                "C",
+                "Java",
+                "Python",
+                "JavaScript",
+                "HTML",
+                "CSS",
+                "Dart",
+                "Sass",
+                "PHP",
+                "SQL",
+                "C++",
+                "Markdown",
+                "Shell",
+            ],
+        },
+        {
+            name: "Frameworks",
+            content: [
+                "ReactJs",
+                "Flutter",
+                "Django",
+                "Django Rest Framework",
+                "Bootstrap",
+                "Material-UI",
+            ],
+        },
+        {
+            name: "Libraries",
+            content: ["Jquery", "JavaFX"],
+        },
+
+        {
+            name: "Technologies",
+            content: [
+                "Github",
+                "Github Actions",
+                "Firebase",
+                "netlify",
+                "Heroku",
+                "Github Pages",
+            ],
+        },
+        { name: "Tools", content: ["Git", "XAMPP", "Android Studio", "ngrok"] },
+    ],
+};
+
+export default about;
